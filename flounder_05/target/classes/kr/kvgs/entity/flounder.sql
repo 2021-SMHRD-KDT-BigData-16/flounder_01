122c173447609c4f60ea5bc6e1424b64c28995b4
김권민 <rnjsals9511@naver.com>
2023-05-30 10:06:44 +0900

    권민 flounder05 쿼리 추가

--- flounder_05/target/classes/kr/kvgs/entity/flounder.sql
+++ flounder_05/target/classes/kr/kvgs/entity/flounder.sql
@@ -261,10 +261,18 @@ select d.dd_id dd_id, d.org_img org_img, d.dt_img dt_img, d.dd_email dd_email,
 insert into detect_dis (dd_email, org_img, dd_comment) values ('mks@kvgs.com', '/DATA/DetectImg/F01_U01_O0001_D2022-07-11_L330_W0483_S2_R05_B01_I00000021.JPG', '배까고 누웠어요');
 
 				
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('림포시스티스', '머리 및 체표 전체의 지느러미에 종양이 생성', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IV001&fshCd=611301', '바이러스성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('바이러스성 신경괴사증', '신경세포에 감염되어 신경조직을 괴사시켜 대량폐사를 유발하고 수직감염에 의해 전염', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IV007&fshCd=611301', '바이러스성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('바이러스성 출혈성 패혈증 바이러스', '체색흑화, 복수저류로 인한 복부팽만과 탈장, 아가미 퇴색 등이 관찰', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IV009&fshCd=611301', '바이러스성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('백점병', '체표와 지느러미에 미세한 흰점이 관찰되며 심한 경우 아가미 뿐만 아니라 안구 등에서도 흰 점이 나타난다', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IP006&fshCd=611301', '기생충성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('버나바이러스병', '주로 치어기에 발생하며 복부가 팽만되거나 뇌부근에 출혈과 발적증상', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IV005&fshCd=611301', '바이러스성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('비브리오증', '체색의 흑화, 지느러미 출혈 및 결손, 부분적 발적과 궤양 ', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IB005&fshCd=611301', '세균성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('상피증생증', '자어기에 나타나며, 지느러미 및 체표가 흐릿하게 변하고, 특히 지느러미 끝 부분이 말려들어 가는 듯한 증상', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IV002&fshCd=611301', '바이러스성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('스쿠티카병', '표피가 박리되고 근육층이 노출되는 궤양증, 체표 염증, 지느러미 연조 및 꼬리자루 노출, 두부손상', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IP007&fshCd=611301', '기생충성질병');
+insert into dis_search (ds_name, ds_simple, ds_url, ds_cause) values ('에드워드병', '수류를 따라 빙빙 도는 행동, 체색 흑화, 지느러미 및 복부 발적 및 충혈, 복부팽만, 탈장', 'https://www.nifs.go.kr/fishguard/disease01_view?dssCd=IB004&fshCd=611301', '세균성질병');			
 				
 				
-				
-				
+				 
 				
 				
 				
